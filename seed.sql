597b59e8d94022978c2ae5a6d53d50f875861d13
mgamatero <mgamatero@yahoo.com>
2018-05-06 00:14:06 -0700

    Fixed seed.sql, bagsmodel, newbags by adding bought_by.  Fixed routes by adding res.json on post

--- seed.sql
+++ seed.sql
@@ -2,19 +2,16 @@ use bags_db;
 
 
 INSERT INTO bags 
-SET id="", name="Prada", model="Elektra", quantity=1, price=2210.00, color="Fire Engine Red", material="Calf leather", SKU = "1BD120_2BB0_F0C9F_V_OBH", image="https://assets.prada.com/content/dam/prada-ecommerce/images/details/1BD120_2BB0_F0C9F_V_OBH-1.jpg/_jcr_content/renditions/original", description= "Calf leather. Detachable studded leather shoulder strap. Studded strap with buckle on the front. Push-lock clasp.", sold = "FALSE";
+SET id="", name="Prada", model="Elektra", quantity=1, price=2210.00, color="Fire Engine Red", material="Calf leather", SKU = "1BD120_2BB0_F0C9F_V_OBH", image="https://assets.prada.com/content/dam/prada-ecommerce/images/details/1BD120_2BB0_F0C9F_V_OBH-1.jpg/_jcr_content/renditions/original", description= "Calf leather. Detachable studded leather shoulder strap. Studded strap with buckle on the front. Push-lock clasp.", sold = "FALSE", bought_by="x";
 
 INSERT INTO bags 
-SET id="", name="Prada", model="Elktra Shoulder", quantity=1, price=2840.00, color="black", material="Saffiano leather", SKU = "1BA179_2BB0_F0D9A_V_OBH", image="https://assets.prada.com/content/dam/prada-ecommerce/images/details/1BA179_2BB0_F0D9A_V_OBH-1.jpg/_jcr_content/renditions/original", description= "Saffiano leather. Studded leather handle. Detachable leather shoulder strap. Polished steel-finish metal hardware.", sold = "FALSE";
+SET id="", name="Prada", model="Elktra Shoulder", quantity=1, price=2840.00, color="black", material="Saffiano leather", SKU = "1BA179_2BB0_F0D9A_V_OBH", image="https://assets.prada.com/content/dam/prada-ecommerce/images/details/1BA179_2BB0_F0D9A_V_OBH-1.jpg/_jcr_content/renditions/original", description= "Saffiano leather. Studded leather handle. Detachable leather shoulder strap. Polished steel-finish metal hardware.", sold = "FALSE", bought_by="x";
 
 INSERT INTO bags 
-SET id="", name="Balenciaga", model="Classic Mettalic Edge City S Bag", quantity=1, price=2050.00, color="Metallic-blue", material="Leather", SKU = "44468171368", image="https://assetsprx.matchesfashion.com/img/product/1391/1179365_1.jpg", description= "Electric-blue naturally creased lamb leather brings a jolt of personality to Balenciaga's Classic Metallic Edge City S bag. It's crafted in Italy with a flat base and two braided top handles, alongside a detachable shoulder strap for added styling versatility. Hallmark brass-tone metal studs and buckle patches complete the look. Contrast it against a backdrop of modern shirting and a denim jacket", sold = "FALSE";
+SET id="", name="Chanel", model="2.55 Reissue Classic Double Flap", quantity=1, price=2069.84, color="Navy-Blue", material="Leather", SKU = "44468171368", image="https://item5.tradesy.com/images/chanel-255-reissue-classic-double-flap-quilted-cc-logo-small-med-navy-blue-lambskin-leather-shoulder-21222444-0-1.jpg?width=440&height=440", description= "Chanel Classic 2.55 Flap Bag in Quilted Navy Blue Lambskin Leather with Gold Hardware GHW in Small Medium size with Double flaps. This stylish shoulder bag is crafted of soft and smooth diamond quilted Navy lambskin leather. There are gold chain link shoulder straps threaded with navy leather. ", sold = "FALSE", bought_by="x";
 
 INSERT INTO bags 
-SET id="", name="Chanel", model="2.55 Reissue Classic Double Flap", quantity=1, price=2069.84, color="Navy-Blue", material="Leather", SKU = "44468171368", image="https://item5.tradesy.com/images/chanel-255-reissue-classic-double-flap-quilted-cc-logo-small-med-navy-blue-lambskin-leather-shoulder-21222444-0-1.jpg?width=440&height=440", description= "Chanel Classic 2.55 Flap Bag in Quilted Navy Blue Lambskin Leather with Gold Hardware GHW in Small Medium size with Double flaps. This stylish shoulder bag is crafted of soft and smooth diamond quilted Navy lambskin leather. There are gold chain link shoulder straps threaded with navy leather. ", sold = "FALSE";
-
-INSERT INTO bags 
-SET id="", name="UCI Bootcamp Student Bag", model="Universal Plastic", quantity=1, price=00.10, color="White", material="Plastic", SKU = "No one cares", image="https://images-na.ssl-images-amazon.com/images/I/81sbXw5yIAL._SY355_.jpg", description= "Hang in there buddy.", sold = "FALSE";
+SET id="", name="UCI Bootcamp Student Bag", model="Universal Plastic", quantity=1, price=00.10, color="White", material="Plastic", SKU = "No one cares", image="https://images-na.ssl-images-amazon.com/images/I/81sbXw5yIAL._SY355_.jpg", description= "Hang in there buddy.", sold = "FALSE", bought_by="x";
 
 
 -- SELECT * FROM bags_db.bags;
